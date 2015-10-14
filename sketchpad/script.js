117e222ae15ef48f005ae5056d266e5c2d0da845
Rohan Goel <rohangoel0296@gmail.com>
2015-10-14 22:25:07 +0530

    Reg/Color Added

--- sketchpad/script.js
+++ sketchpad/script.js
@@ -5,30 +5,39 @@ $(document).ready(function(){
     	$("#heading").text(name+"'s SketchPad");
     };
 
-    var size = $("#size").val();
-    var pixel_size = (480/size)-2; 	//to accomadate borders
 
     $('#size').on('change', function() {
 	  	$("#board").empty();
 	  	
-	  	size = $("#size").val();
-		pixel_size = (480/size)-2;	//to accomadate borders
+	  	var size = $("#size").val();
+		var pixel_size = (480/size)-2;	//to accomadate borders
 	    
 	    for (var i = 0; i < size; i++) {
 	   		
 	    	for (var j = 0; j < size; j++) {
-	   			$('#board').append($("<div></div>").addClass("pixel").css({"height":pixel_size,"width":pixel_size, "display": "inline-block"}));
+	   			$('#board').append($("<div></div>")
+	   				.attr({	onmouseleave:"mouseLeavePixelHandler(this)",
+	   						onmouseenter:"mouseEnterPixelHandler(this)"})
+	   				.addClass("pixel")
+	   				.css({"height":pixel_size,"width":pixel_size, "display": "inline-block"}));
 	    	 };
-
-	    	 //$('#board').append("<br>");
 	    };
 	});
+
 
 	$('#reset').on('click',function(){
 		$("#board").empty();
 		$("#size").val('0');
-
-
 	});
 
+	
 });
+
+function mouseEnterPixelHandler(obj){
+		$(obj).css("background-color","green");
+	}
+
+function mouseLeavePixelHandler (obj) {
+		$(obj).css("background-color","yellow");
+	}
+
