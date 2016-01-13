bee85fcf72575963678cf4247b1930672c983381
Rohan Goel <rohangoel0296@gmail.com>
2016-01-14 01:04:56 +0530

    Started adding elements from jQuery instead of JavaScript

--- my-restaurant-tabbed/js/script.js
+++ my-restaurant-tabbed/js/script.js
@@ -1,9 +1,31 @@
 $(document).ready(function() {
 	
-	addHeader();
+	addHeaderAndFooter();
+	addImage();
 
 });
 
 function addToContent(childElement) {
 	$('#content').append(childElement);
 };
+
+function addHeaderAndFooter() {
+	var header = $("<div id='header'></div>");
+	var headerText = $("<h1></h1>").text('Rohan\'s Pizza Corner');
+	var footer = $("<div id='footer'></div>");
+	var footerText = $("<p>Get the best pizza in town</p>");
+	var footerText2 = $('<p style="float:right">At the most affordable prices</p>');
+
+	addToContent(header);
+	header.append(headerText);
+	addToContent(footer);
+	footer.append(footerText);
+	footer.append(footerText2);
+};
+
+function addImage() {
+	var image = $("<div id='image'></div>");
+	addToContent(image);
+};
+				
+			
